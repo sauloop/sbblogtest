a45898d5e157126aaa1b3cb6fa94fec81015f98d
Pablo Giraldo <pablogirald@yahoo.es>
2020-05-27 12:28:44 +0200

    Change security config

--- src/main/java/info/pablogiraldo/sbblog/controller/ArticleController.java
+++ src/main/java/info/pablogiraldo/sbblog/controller/ArticleController.java
@@ -75,7 +75,7 @@ public class ArticleController {
 
 //			String ruta = "C://pruebas//img";
 
-			String relativeWebPath = "/static/img/";
+			String relativeWebPath = "";
 			String ruta = context.getRealPath(relativeWebPath);
 
 			String nombreUnico = UUID.randomUUID().toString() + "-" + foto.getOriginalFilename();
