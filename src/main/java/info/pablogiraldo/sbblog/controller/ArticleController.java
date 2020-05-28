47498ec5f1bdfe8219dfd601e88f84f4613153e2
Pablo Giraldo <pablogirald@yahoo.es>
2020-05-28 12:40:52 +0200

    Test

--- src/main/java/info/pablogiraldo/sbblog/controller/ArticleController.java
+++ src/main/java/info/pablogiraldo/sbblog/controller/ArticleController.java
@@ -77,7 +77,7 @@ public class ArticleController {
 //			String ruta = "C://pruebas//img";
 
 //			String relativeWebPath = "";
-			String relativeWebPath = "src/main/resources/";
+			String relativeWebPath = "file:/opt/files/";
 			String ruta = context.getRealPath(relativeWebPath);
 
 //			String nombreUnico = UUID.randomUUID().toString() + "-" + foto.getOriginalFilename();
