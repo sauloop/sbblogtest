6204e5b28bd92dfea490b46348f1ca417a16b9f8
Pablo Giraldo <pablogirald@yahoo.es>
2020-06-06 12:53:13 +0200

    Change img config

--- src/main/java/info/pablogiraldo/sbblog/MvcConfig.java
+++ src/main/java/info/pablogiraldo/sbblog/MvcConfig.java
@@ -22,7 +22,7 @@ public class MvcConfig implements WebMvcConfigurer {
 
 //		String ruta = context.getRealPath("");
 
-		registry.addResourceHandler("/img/**").addResourceLocations("classpath:/images/");
+		registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
 
 	}
 
